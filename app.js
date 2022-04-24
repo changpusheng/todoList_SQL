a916d676defa7d36afcab821726b06492523667f
pusheng <sdf753741789@gmail.com>
2022-04-24 23:07:27 +0800

    set login and register

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const exphbs = require('express-handlebars').engine
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const Port = 3000
+const routes = require('./routes')
 
 app.engine('hbs' ,exphbs({defaultLayout:'main',extname:'hbs'}))
 app.set('view engine','hbs')
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('method'))
-
-app.get('/',(req,res) =>{
-  res.send('hello world')
-})
+app.use(routes)
 
 app.listen(Port,() =>{
   console.log(`This server is running on http://localhost:${Port}`)
