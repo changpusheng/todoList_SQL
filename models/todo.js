ba05f5bf5f106f8eeccb20fc64c47138ed6ef58c
pusheng <sdf753741789@gmail.com>
2022-04-25 14:27:34 +0800

    add sequelize migrate

---
+++ models/todo.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const ToDo = sequelize.define('ToDo', {
+    name: DataTypes.STRING,
+    isDone: DataTypes.BOOLEAN
+  }, {});
+  ToDo.associate = function(models) {
+    // associations can be defined here
+  };
+  return ToDo;
+};
